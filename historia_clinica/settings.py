5caa68eaa9d6c275c66e3eb2798ca11a5f919ac8
AlejandroFeriaGonzalez <aferiag@unal.edu.co>
2024-06-11 19:52:39 -0500

    fix

--- historia_clinica/settings.py
+++ historia_clinica/settings.py
@@ -22,13 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e-6=m6%9f0r_l+(l)1vg!#_rlvetw85l=5upo99so8y5w&+g9c"
+local_key = "django-insecure-e-6=m6%9f0r_l+(l)1vg!#_rlvetw85l=5upo99so8y5w&+g9c"
+SECRET_KEY = os.environ.get('SECRET_KEY', default=local_key)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
