501b4f53763fc66db46ae9448c63b4d70563931e
AlejandroFeriaGonzalez <aferiag@unal.edu.co>
2024-06-13 10:43:35 -0500

    remove whitenoise

--- historia_clinica/settings.py
+++ historia_clinica/settings.py
@@ -69,7 +69,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django_browser_reload.middleware.BrowserReloadMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "historia_clinica.urls"
