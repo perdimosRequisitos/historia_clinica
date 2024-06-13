d7c7a83f9627f7d09d155a5ff408c48e147767a2
AlejandroFeriaGonzalez <aferiag@unal.edu.co>
2024-06-13 09:41:35 -0500

    a

--- historia_clinica/settings.py
+++ historia_clinica/settings.py
@@ -29,7 +29,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default=os.environ.get("MY_SECRET_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
-DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
